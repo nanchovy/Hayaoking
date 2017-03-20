66a02c5c77e7e1c1d05ec15b0f10e14629fdc8d1
naoyukinagayasu <cyouan927@gmail.com>
2017-03-20 19:25:38 +0900

    新規作成Xcode Project

---
+++ Hayaoking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hayaoking
+//
+//  Created by 長安尚之 on 2017/03/20.
+//  Copyright © 2017年 長安尚之. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
